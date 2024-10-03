0d9ae7ecddbd9b4b54c99a62c976ce3990089e58
Ashrafee <dev.ashrafee@gmail.com>
2024-10-03 22:02:34 +0600

    Project finished

--- script/script.js
+++ script/script.js
@@ -21,10 +21,32 @@ depositBtn.addEventListener("click", function () {
   const totalDeposit = currentDepositNumber + depositNumber;
 
   document.getElementById("current-deposit").innerText = totalDeposit;
+  document.getElementById("deposit-amount").value = "";
 
   const currentBalance = document.getElementById("current-balance").innerText;
   const currentBalanceNumber = parseFloat(currentBalance);
   const totalBalance = currentBalanceNumber + depositNumber;
 
   document.getElementById("current-balance").innerText = totalBalance;
+});
+
+// Withdraw
+
+const withdrawBtn = document.getElementById("withdraw-btn");
+withdrawBtn.addEventListener("click", function () {
+  const withdrawAmount = document.getElementById("withdraw-amount").value;
+  const withdrawNumber = parseFloat(withdrawAmount);
+
+  const currentWithdraw = document.getElementById("current-withdraw").innerText;
+  const currentWithdrawNumber = parseFloat(currentWithdraw);
+  const totalWithdraw = currentWithdrawNumber + withdrawNumber;
+
+  document.getElementById("current-withdraw").innerText = totalWithdraw;
+
+  const currentBalance = document.getElementById("current-balance").innerText;
+  const currentBalanceNumber = parseFloat(currentBalance);
+  const totalBalance = currentBalanceNumber - withdrawNumber;
+
+  document.getElementById("current-balance").innerText = totalBalance;
+  document.getElementById("withdraw-amount").value = "";
 });
